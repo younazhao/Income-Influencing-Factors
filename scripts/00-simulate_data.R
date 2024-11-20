02246d49e6af189b6b072e9ec147140f31e1755e
younazhao <youna.zhao@mail.utoronto.ca>
2024-11-20 16:10:43 -0500

    Update the simulated data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,51 +1,67 @@
 #### Preamble ####
-# Purpose: Simulates a dataset of Australian electoral divisions, including the 
-  #state and party that won each division.
+# Purpose: Simulates a dataset of Survey of Consumer Finances
 # Author: Wen Han Zhao
 # Date: 1 December 2024
 # Contact: youna.zhao@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: The `tidyverse` package must be installed
-
+# Pre-requisites: The `tidyverse` and `arrow` package must be installed
 
 #### Workspace setup ####
-library(tidyverse)
-set.seed(853)
 
+library(tidyverse)
+library(arrow)
+set.seed(853)
+num_simulated_data <- 1000
 
 #### Simulate data ####
-# State names
-states <- c(
-  "New South Wales",
-  "Victoria",
-  "Queensland",
-  "South Australia",
-  "Western Australia",
-  "Tasmania",
-  "Northern Territory",
-  "Australian Capital Territory"
-)
 
-# Political parties
-parties <- c("Labor", "Liberal", "Greens", "National", "Other")
+# Sex 
+sex <- c("Female", "Male")
+
+# Age
+age <- sample(18:95, size = num_simulated_data, replace = TRUE)
+
+# Weight 
+weight <- runif(num_simulated_data, min = 2.754882, max = 28265.28)
+
+# Education
+Education <- c("Less than 1st grade", "1st, 2nd, 3rd, or 4th grade", "5th or 6th grade", "7th or 8th grade",
+               "9th grade", "10th grade", "11th grade", "12th grade, no diploma", 
+               "High school graduate - high school diploma or equivalent", "Some college but no degree",
+               "Associate degree in college - Occupation/vocation program", 
+               "Associate degree in college - academic program", "Bachelor's degree", "Master's degree",
+               "Professional school degree and Doctorate degree")
+
+# Marrital Status
+Marrital_status <- c("Married", "Not Married")
+
+# Work Status
+Work_status <- c("Work for someone else", "Self-employed/partnership", "Retired/Disabled", "Not working")
+
+# Occupation Classification
+Occupation <- c("Managerial/Professional", "Technical/Sales/Services", "Other (production workers,farmers)",
+                "Not working")
+
+# Income (Actual mean and sd from the dataset)
+Income <- rnorm(num_simulated_data, mean = 1606631, sd = 12495518)
+Income <- pmax(Income, 0) 
 
 # Create a dataset by randomly assigning states and parties to divisions
-analysis_data <- tibble(
-  division = paste("Division", 1:151),  # Add "Division" to make it a character
-  state = sample(
-    states,
-    size = 151,
-    replace = TRUE,
-    prob = c(0.25, 0.25, 0.15, 0.1, 0.1, 0.1, 0.025, 0.025) # Rough state population distribution
-  ),
-  party = sample(
-    parties,
-    size = 151,
-    replace = TRUE,
-    prob = c(0.40, 0.40, 0.05, 0.1, 0.05) # Rough party distribution
-  )
+simulated_data <- tibble(
+  ID = sample(11:46035, size = num_simulated_data),
+  sex = sample(sex, size = num_simulated_data, replace = TRUE, prob = c(0.4, 0.6)),
+  age = age,
+  weight = weight, 
+  Education = sample(Education, size = num_simulated_data, replace = TRUE),
+  Marrital_status = sample(Marrital_status, size = num_simulated_data, replace = TRUE),
+  Work_status = sample(Work_status, size = num_simulated_data, replace = TRUE),
+  Occupation = sample(Occupation, size = num_simulated_data, replace = TRUE),
+  Income = Income
 )
 
+# arrange the simulated data by ID 
+simulated_data <- simulated_data %>%
+  arrange(ID)
 
 #### Save data ####
-write_csv(analysis_data, "data/00-simulated_data/simulated_data.csv")
+write_parquet(simulated_data, "data/00-simulated_data/simulated_data.parquet")
